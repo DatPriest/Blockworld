61332fc75a8df8f767c14aa54bd406d11e2f240b
Leon Cwiertnia <lcwiertnia@web.de>
2021-03-06 14:30:18 +0100

    Next

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+
+	public MeshRenderer meshRenderer;
+	public MeshFilter meshFilter;
+
+	int vertexIndex = 0;
+	List<Vector3> vertices = new List<Vector3>();
+	List<int> triangles = new List<int>();
+	List<Vector2> uvs = new List<Vector2>();
+
+	bool[,,] voxelMap = new bool[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];
+	// Start is called before the first frame update
+	void Start()
+	{
+
+
+		PopulateVoxelMap();
+		CreateMeshData();
+		CreateMesh();
+
+	}
+	void CreateMeshData()
+	{
+		for (int y = 0; y < VoxelData.ChunkHeight; y++)
+		{
+			for (int x = 0; x < VoxelData.ChunkWidth; x++)
+			{
+				for (int z = 0; z < VoxelData.ChunkWidth; z++)
+				{
+					AddVoxelDataToChunk(new Vector3(x, y, z));
+				}
+			}
+		}
+	}
+
+	bool CheckVoxel(Vector3 pos)
+	{
+		int x = Mathf.FloorToInt(pos.x);
+		int y = Mathf.FloorToInt(pos.y);
+		int z = Mathf.FloorToInt(pos.z);
+
+		if (x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > VoxelData.ChunkHeight - 1 || z < 0 || z > VoxelData.ChunkWidth - 1)
+			return false;
+
+		return voxelMap[x, y, z];
+	}
+
+	void AddVoxelDataToChunk(Vector3 pos)
+	{
+		for (int p = 0; p < 6; p++)
+		{
+			if (!CheckVoxel(pos + VoxelData.faceChecks[p]))
+			{
+				vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 0]]);
+				vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 1]]);
+				vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 2]]);
+				vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 3]]);
+
+				triangles.Add(vertexIndex);
+				triangles.Add(vertexIndex + 1);
+				triangles.Add(vertexIndex + 2);
+				triangles.Add(vertexIndex + 2);
+				triangles.Add(vertexIndex + 1);
+				triangles.Add(vertexIndex + 3);
+				vertexIndex += 4;
+			}
+		}
+	}
+
+	void PopulateVoxelMap()
+	{
+
+		for (int y = 0; y < VoxelData.ChunkHeight; y++)
+		{
+			for (int x = 0; x < VoxelData.ChunkWidth; x++)
+			{
+				for (int z = 0; z < VoxelData.ChunkWidth; z++)
+				{
+					voxelMap[x, y, z] = true;
+				}
+			}
+		}
+
+	}
+
+	void CreateMesh()
+	{
+
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.uv = uvs.ToArray();
+
+		mesh.RecalculateNormals();
+
+		meshFilter.mesh = mesh;
+
+	}
+}
